715b6b0e2405d71a578c4e5563cd89bc5ca95ce5
parabola949 <parabola949@gmail.com>
2016-06-30 15:08:11 -0500

    Only download image if it's new

--- Werewolf for Telegram/Werewolf Node/Helpers/ImageHelper.cs
+++ Werewolf for Telegram/Werewolf Node/Helpers/ImageHelper.cs
@@ -15,43 +15,52 @@ namespace Werewolf_Node.Helpers
         internal static string ImagePath = @"c:\inetpub\werewolf\Images\";
         public static async void GetUserImage(int userid)
         {
-            var photos = await Program.Bot.GetUserProfilePhotos(userid, limit: 1);
-            if (photos.Photos.Length == 0) return;//nada
-            var sizes = photos.Photos[0];
-            var id = "";
-            var largest = 0;
-            foreach (var s in sizes)
-            {
-                if (s.FileSize > largest)
-                    id = s.FileId;
-                
-            }
-
-            if (String.IsNullOrEmpty(id))
-            {
-                return;
-            }
-
-            var file = await Program.Bot.GetFile(id);
-            var photoPath = file.FilePath;
-            var fileName = photoPath.Substring(photoPath.LastIndexOf("/") + 1);
-            var uri = $"https://api.telegram.org/file/bot{Program.APIToken}/{photoPath}";
-
-            //write the new info to the database
             using (var db = new WWContext())
             {
                 var p = db.Players.FirstOrDefault(x => x.TelegramId == userid);
                 if (p != null)
                 {
+                    var photos = await Program.Bot.GetUserProfilePhotos(userid, limit: 1);
+                    if (photos.Photos.Length == 0) return;//nada
+                    var sizes = photos.Photos[0];
+                    var id = "";
+                    var largest = 0;
+                    foreach (var s in sizes)
+                    {
+                        if (s.FileSize > largest)
+                            id = s.FileId;
+
+                    }
+
+                    if (String.IsNullOrEmpty(id))
+                    {
+                        return;
+                    }
+
+
+
+                    var file = await Program.Bot.GetFile(id);
+                    var photoPath = file.FilePath;
+                    var fileName = photoPath.Substring(photoPath.LastIndexOf("/") + 1);
+                    //check that the file name is different
+                    if (p.ImageFile == fileName) 
+                        return; //same, no reason to download again
+
+                    var uri = $"https://api.telegram.org/file/bot{Program.APIToken}/{photoPath}";
+
+                    //write the new info to the database
+
+                    
+
+
+                    //now download the photo to the path
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(new Uri(uri), ImagePath + fileName);
+                    }
                     p.ImageFile = fileName;
                     db.SaveChanges();
                 }
-            }
-
-            //now download the photo to the path
-            using (var client = new WebClient())
-            {
-                client.DownloadFile(new Uri(uri), ImagePath + fileName);
             }
         }
     }
