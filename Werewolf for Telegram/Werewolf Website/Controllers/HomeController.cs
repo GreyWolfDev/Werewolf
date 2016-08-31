30a9dd6fb4c552a5cb0ee7152562beedef6021e4
parabola949 <parabola949@gmail.com>
2016-08-31 13:58:04 -0500

    stuff. just, stuff.

--- Werewolf for Telegram/Werewolf Website/Controllers/HomeController.cs
+++ Werewolf for Telegram/Werewolf Website/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Database;
 
 namespace Werewolf_Website.Controllers
 {
@@ -10,21 +11,57 @@ namespace Werewolf_Website.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Title = "Werewolf Telegram";
+            ViewBag.Description = "Werewolf for Telegram. Play werewolf while chatting with your friends on Telegram!";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Title = "Werewolf Telegram";
+            ViewBag.Message = "Information about Werewolf for Telegram";
+            ViewBag.Description = "Werewolf for Telegram. Play werewolf while chatting with your friends on Telegram!";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Title = "Werewolf Telegram";
+            ViewBag.Message = "Contact information";
+            ViewBag.Description = "Werewolf for Telegram. Play werewolf while chatting with your friends on Telegram!";
 
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetBotStatus()
+        {
+            using (var DB = new WWContext())
+            return Json(DB.BotStatus.ToList().Select(x => new { Name = x.BotName, Status = x.BotStatus, Link = x.BotLink }).ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpGet]
+        public JsonResult GetPublicGroups()
+        {
+            using (var DB = new WWContext())
+            {
+                var pubGroups = (from g in DB.Groups
+                    where g.Preferred == true & !String.IsNullOrEmpty(g.UserName)
+                    select
+                        new
+                        {
+                            Name = g.Name,
+                            Language = g.Language,
+                            UserName = g.UserName,
+                            Link = "http://telegram.me/" + g.UserName
+                        }).ToList();
+
+                //var result = pubGroups.Aggregate("", (current, group) => current + $"<tr><td>{group.Name}</td><td>{group.Language}</td><td>{group.Link}</td></tr>");
+                //var table = @"<table class=""table table-hover table-responsive""><thead><tr><th>Name</th><th>Language / Region</th><th>Link</th></tr></thead><tbody>" + result + "</tbody></table>";
+                return Json(pubGroups, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
