0fdb12a12dbc8db5bced6c437b8924fc2f8bf134
parabola949 <parabola949@gmail.com>
2016-10-05 11:52:13 -0500

    Testing website receive from github

--- Werewolf for Telegram/BuildAutomation/App_Start/WebApiConfig.cs
+++ Werewolf for Telegram/BuildAutomation/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace BuildAutomation
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.InitializeReceiveGitHubWebHooks();
         }
     }
 }
---
+++ Werewolf for Telegram/BuildAutomation/GitHubHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using BuildAutomation.Models;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BuildAutomation
+{
+    public class GitHubHandler : WebHookHandler
+    {
+        
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            string action = context.Actions.First();
+            JObject data = context.GetDataOrDefault<JObject>();
+            //var push = JsonConvert.DeserializeObject<PushEvent>(data.ToString());
+            //write this to App_Data so we can verift
+            string path = HttpContext.Current.Server.MapPath("~/App_Data/last.json");
+            using (var sw = new StreamWriter(path))
+            {
+                sw.WriteLine(data);
+            }
+            return Task.FromResult(true);
+        }
+    }
+}
