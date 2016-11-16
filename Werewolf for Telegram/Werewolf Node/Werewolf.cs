f459373cb1e975c2d74d0e1d35a012b1e173f296
renyhp <renyhp@hotmail.it>
2016-11-16 22:51:37 +0100

    Fix role assigning.. again

--- Werewolf for Telegram/Werewolf Node/Werewolf.cs
+++ Werewolf for Telegram/Werewolf Node/Werewolf.cs
@@ -1074,6 +1074,12 @@ namespace Werewolf_Node
                     }
 
 
+                    //make sure that we have at least two teams
+                    if (! (
+                        rolesToAssign.Any(x => !nonVgRoles.Contains(x)) //make sure we have VGs
+                            && rolesToAssign.Any(r => nonVgRoles.Contains(r) && r != IRole.Sorcerer && r != IRole.Tanner) //make sure we have at least one enemy
+                            )) 
+                        continue; //redo role assignment. better to rely on randomness, than trying to fix it
 
                     //the roles to assign are good, now if it's not a chaos game we need to check if they're balanced
                     if (!Chaos)
@@ -1094,7 +1100,7 @@ namespace Werewolf_Node
                             balanced = true;
                         //else, redo role assignment. better to rely on randomness, than trying to fix it
                     }
-                } while (!balanced);
+                } while (!balanced && !Chaos);
 
 
                 //shuffle things
