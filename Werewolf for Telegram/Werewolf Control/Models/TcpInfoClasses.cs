9a4ee46d0b579534d030bf56575e011993e55553
renyhp <renyhp@hotmail.it>
2016-11-21 16:58:17 +0100

    Add /extend command

--- Werewolf for Telegram/Werewolf Control/Models/TcpInfoClasses.cs
+++ Werewolf for Telegram/Werewolf Control/Models/TcpInfoClasses.cs
@@ -116,4 +116,12 @@ namespace Werewolf_Control.Models
         public string JType { get; set; } = "GameKillInfo";
         public long GroupId { get; set; }
     }
+
+    public class ExtendTimeInfo
+    {
+        public string JType { get; set; } = "ExtendTimeInfo";
+        public long GroupId { get; set; }
+        public long User { get; set; }
+        public int Seconds { get; set; }
+    }
 }
--- Werewolf for Telegram/Werewolf Node/Program.cs
+++ Werewolf for Telegram/Werewolf Node/Program.cs
@@ -218,6 +218,11 @@ namespace Werewolf_Node
                                 };
                                 message.Reply(JsonConvert.SerializeObject(gi));
                                 break;
+                            case "ExtendTimeInfo":
+                                var eti = JsonConvert.DeserializeObject<ExtendTimeInfo>(msg);
+                                game = Games.FirstOrDefault(x => x.ChatId == eti.GroupId);
+                                game?.ExtendTime(eti.User, eti.Seconds);
+                                break;
                             default:
                                 Console.WriteLine(msg);
                                 break;
