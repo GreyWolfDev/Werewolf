24d51e5589635b093bbea5004059c222a23f00fe
Fernando Barros <fernando.tbarros@gmail.com>
2016-12-14 07:14:30 -0200

    Criando tabela ImageLanguage para armazenar o GIF de cada lingua

--- Werewolf for Telegram/Database/WerewolfModel.Designer.cs
+++ Werewolf for Telegram/Database/WerewolfModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Werewolf\Werewolf for Telegram\Database\WerewolfModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Programacao\C#\Werewolf\Werewolf for Telegram\Database\WerewolfModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
