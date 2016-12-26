433279b02fd43e4896ed6aeac6fd9d7b5a4ba668
renyhp <renyhp@hotmail.it>
2016-12-26 11:58:45 +0100

    Fix bot not sending messages containing <, >, &

--- Werewolf for Telegram/Werewolf Node/Program.cs
+++ Werewolf for Telegram/Werewolf Node/Program.cs
@@ -15,6 +15,7 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using Werewolf_Node.Models;
+using Werewolf_Node.Helpers;
 using Message = TcpFramework.Message;
 
 namespace Werewolf_Node
@@ -271,6 +272,7 @@ namespace Werewolf_Node
         internal static async Task<Telegram.Bot.Types.Message> Send(string message, long id, bool clearKeyboard = false, InlineKeyboardMarkup customMenu = null, Werewolf game = null)
         {
             MessagesSent++;
+            message = message.FormatHTML();
             //message = message.Replace("`",@"\`");
             if (clearKeyboard)
             {
