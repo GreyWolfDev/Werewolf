1b1269a383cc4f43eed98fdd7829d06a87b31ddd
Justin Mangum <parabola949@gmail.com>
2016-12-27 17:15:28 -0600

    Merge beta to master (#203)
    * Beginning more granular build definitions
    
    * Fix two cults breaking the game again (oops)
    
    (and revert previous fix because that wasn't the problem, and it can
    actually hurt more than expected)
    
    * Have control reply back on build buttons
    
    * Another commit to fix + test commit automation
    
    * More build automation
    
    * Build is now automated.  Will Fix up release later
    
    * Add compare link to commit
    
    * FInal keyboard options for build definitions

--- Werewolf for Telegram/BuildAutomation/Controllers/BuildController.cs
+++ Werewolf for Telegram/BuildAutomation/Controllers/BuildController.cs
@@ -23,75 +23,165 @@ namespace BuildAutomation.Controllers
 {
     public class BuildController : ApiController
     {
-
-        public void Post()
+        [HttpPost]
+        public HttpResponseMessage Post()
         {
+            string TelegramAPIKey = ConfigurationManager.AppSettings.Get("TelegramAPIToken");
+            var bot = new Telegram.Bot.Client(TelegramAPIKey, System.Environment.CurrentDirectory);
             try
             {
-                var obj = JsonConvert.DeserializeObject<ReleaseEvent>(Request.Content.ReadAsStringAsync().Result);
-                var releaseKey = ConfigurationManager.AppSettings.Get("VSTSReleaseId");
-                var buildKey = ConfigurationManager.AppSettings.Get("VSTSBuildId");
-                if (obj.subscriptionId == releaseKey) //can't have random people triggering this!
+                var body = Request.Content.ReadAsStringAsync().Result;
+                var obj = JsonConvert.DeserializeObject<ReleaseEvent>(body);
+                if (obj?.subscriptionId != null)
                 {
-                    string TelegramAPIKey = ConfigurationManager.AppSettings.Get("TelegramAPIToken");
-
-                    var msg = obj.detailedMessage.markdown;
-                    InlineKeyboardMarkup menu = null;
-                    if (obj.resource.environment.status == "succeeded")
+                    var releaseKey = ConfigurationManager.AppSettings.Get("VSTSReleaseId");
+                    var buildKey = ConfigurationManager.AppSettings.Get("VSTSBuildId");
+                    if (obj.subscriptionId == releaseKey) //can't have random people triggering this!
                     {
-                        msg += "\nDo you want me to copy the files and update?";
-                        menu = new InlineKeyboardMarkup(new[]
+                        //what was released
+                        var beta = obj.resource.environment.name.Contains("Beta");
+                        var node = obj.resource.environment.name.Contains("Node");
+
+                        
+
+                        var msg = obj.detailedMessage.markdown;
+                        InlineKeyboardMarkup menu = null;
+                        if (obj.resource.environment.status == "succeeded")
                         {
-                            new []
-                            {
-                                new InlineKeyboardButton("Beta - Control", "update|betacontrol"),
-                                new InlineKeyboardButton("All - Control", "update|allcontrol"),
-                            },
-                            new []
-                            {
-                                new InlineKeyboardButton("Beta - Nodes", "update|betanodes"),
-                                new InlineKeyboardButton("All - Nodes", "update|allnodes"),
-                            },
-                            new []
-                            {
-                                new InlineKeyboardButton("Beta - Both", "update|betaboth"),
-                                new InlineKeyboardButton("All - Both", "update|allboth")
-                            },
-                            new []
-                            {
-                                new InlineKeyboardButton("Do Nothing", "update|no"),
-                            }
-                        });
+                            msg += "\nDo you want me to copy the files and update?";
+                            menu = new InlineKeyboardMarkup(new [] {new InlineKeyboardButton("Yes", $"update|{(beta?"beta":"release")}{(node?"node":"control")}"), new InlineKeyboardButton("No", "update|no") });
+                        }
+
+                        
+                        bot.SendTextMessage(-1001077134233, msg, replyMarkup: menu, parseMode: ParseMode.Markdown);
                     }
 
-                    var bot = new Telegram.Bot.Client(TelegramAPIKey, System.Environment.CurrentDirectory);
-                    bot.SendTextMessage(-1001077134233, msg, replyMarkup: menu, parseMode: ParseMode.Markdown);
-                }
-
-                if (obj.subscriptionId == buildKey)
-                {
-                    var build = JsonConvert.DeserializeObject<BuildEvent>(Request.Content.ReadAsStringAsync().Result);
-                    string TelegramAPIKey = ConfigurationManager.AppSettings.Get("TelegramAPIToken");
-                    var detail = obj.detailedMessage.markdown;
-                    if (detail.Contains("\r\n+ Process 'msbuild.exe'"))
+                    if (obj.subscriptionId == buildKey)
                     {
-                        detail = detail.Substring(0, detail.IndexOf("\r\n+ Process 'msbuild.exe'"));
+                        var build = JsonConvert.DeserializeObject<BuildEvent>(Request.Content.ReadAsStringAsync().Result);
+
+                        var detail = obj.detailedMessage.markdown;
+                        if (detail.Contains("\r\n+ Process 'msbuild.exe'"))
+                        {
+                            detail = detail.Substring(0, detail.IndexOf("\r\n+ Process 'msbuild.exe'"));
+                        }
+                        detail = detail.Replace("\r\n+ ", "\r\n");
+                        var msg = detail + "\n";
+                        var urlPre = "https://github.com/parabola949/Werewolf/commit/";
+                        msg +=
+                            $"Built with commit [{build.resource.sourceVersion.Substring(0, 7)}]({urlPre + build.resource.sourceVersion}) as latest";
+                        if (build.resource.result == "succeeded")
+                            msg += "\nRelease is now being created, you will be notified when it is completed.";
+                        
+                        bot.SendTextMessage(-1001077134233, msg, parseMode: ParseMode.Markdown);
                     }
-                    detail = detail.Replace("\r\n+ ", "\r\n");
-                    var msg = detail + "\n";
-                    var urlPre = "https://github.com/parabola949/Werewolf/commit/";
-                    msg += $"Build triggered by commit [{build.resource.sourceVersion.Substring(0, 7)}]({urlPre + build.resource.sourceVersion})";
-                    if (build.resource.result == "succeeded")
-                        msg += "\nRelease is now being created, you will be notified when it is completed.";
-                    var bot = new Telegram.Bot.Client(TelegramAPIKey, System.Environment.CurrentDirectory);
-                    bot.SendTextMessage(-1001077134233, msg, parseMode: ParseMode.Markdown);
                 }
+                else
+                {
+                    //github
+                    var push = JsonConvert.DeserializeObject<PushEvent>(body);
+                    var msg =
+                        $"🔨 <a href='{push.compare}'>{push.commits.Length} new commit{(push.commits.Length > 1 ? "s" : "")} to {push.repository.name}:{push._ref}</a>\n\n";
+                    msg = push.commits.Aggregate(msg,
+                        (current, a) => current + $"<a href='{a.url}'>{a.id.Substring(0, 7)}</a>: {a.message} ({a.author.username})\n");
+                    
+
+                   
+
+                    //string path = HttpContext.Current.Server.MapPath("~/App_Data/github.json");
+                    //using (var sw = new StreamWriter(path))
+                    //{
+                    //    foreach (var c in push.commits)
+                    //        sw.WriteLine($"Commit by: {c.committer.username}\nMessage: {c.message}\n");
+                    //    sw.WriteLine(body);
+                    //}
+
+                    //check what was built
+                    var beta = push._ref.Contains("beta"); //beta or master - refs/head/<branch>
+                    //now check if control was changed
+                    var control =
+                        push.commits.Any(
+                            x => x.modified.Union(x.added).Union(x.removed).Any(c => c.Contains("Werewolf Control")));
+                    var node =
+                        push.commits.Any(
+                            x => x.modified.Union(x.added).Union(x.removed).Any(c => c.Contains("Werewolf Node")));
+
+                    if (!control && !node) //nothing to build
+                    {
+                        bot.SendTextMessage(-1001077134233, msg, parseMode: ParseMode.Html, disableWebPagePreview: true);
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+
+
+
+                    var none = new InlineKeyboardButton("No", "build|no");
+                    var yes = "Yes";
+                    var betaControl = new InlineKeyboardButton(yes, "build|betacontrol");
+                    var betaNode = new InlineKeyboardButton(yes, "build|betanode");
+                    var betaBoth = new InlineKeyboardButton(yes, "build|betaboth");
+
+                    var releaseControl = new InlineKeyboardButton(yes, "build|releasecontrol");
+                    var releaseNode = new InlineKeyboardButton(yes, "build|releasenode");
+                    var releaseBoth = new InlineKeyboardButton(yes, "build|releaseboth");
+                    InlineKeyboardMarkup menu;
+
+                    msg += "\nThis commit contains changes to ";
+                    if (beta)
+                    {
+                        if (control && node)
+                        {
+                            menu = new InlineKeyboardMarkup(new[] { betaBoth, none });
+                            msg += "Control and Node";
+                        }
+                        else if (control)
+                        {
+                            menu = new InlineKeyboardMarkup(new[] { betaControl, none });
+                            msg += "Control only";
+                        }
+                        else
+                        {
+                            menu = new InlineKeyboardMarkup(new[] { betaNode, none });
+                            msg += "Node only";
+                        }
+                    }
+                    else
+                    {
+                        if (control && node)
+                        {
+                            menu = new InlineKeyboardMarkup(new[] { releaseBoth, none });
+                            msg += "Control and Node";
+                        }
+                        else if (control)
+                        {
+                            menu = new InlineKeyboardMarkup(new[] { releaseControl, none });
+                            msg += "Control only";
+                        }
+                        else
+                        {
+                            menu = new InlineKeyboardMarkup(new[] { releaseNode, none });
+                            msg += "Node only";
+                        }
+                    }
+                    msg += $", on {(beta ? "Beta" : "Release")}\n";
+                    msg += "Do you want to build?";
+                    bot.SendTextMessage(-1001077134233, msg, replyMarkup: menu, parseMode: ParseMode.Html, disableWebPagePreview: true);
+                    
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
             }
             catch (Exception e)
             {
-                string path = HttpContext.Current.Server.MapPath("~/App_Data/error.log");
-                StreamWriter writer = new StreamWriter(path);
-                writer.WriteLine(e.Message);
+                //string path = HttpContext.Current.Server.MapPath("~/App_Data/error");
+                while (e.InnerException != null)
+                    e = e.InnerException;
+                bot.SendTextMessage(-1001077134233, e.Message + "\n" + e.StackTrace);
+                //using (var sw = new StreamWriter(path))
+                //{
+                //    sw.WriteLine(e.Message);
+                //    sw.Flush();
+                //}
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
             }
 
 
