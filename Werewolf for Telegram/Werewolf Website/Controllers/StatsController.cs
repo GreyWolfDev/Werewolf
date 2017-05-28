0822a62d0ac33d9f5e3147e4e4247cca73dbf027
Olfi01 <flom@meyer-alpers.de>
2017-05-28 19:43:44 +0200

    Revert "Added JSON option for PlayerAchievements"
    This reverts commit 996655373209b9667df24db8af0909a013ff5928.

--- Werewolf for Telegram/Werewolf Website/Controllers/StatsController.cs
+++ Werewolf for Telegram/Werewolf Website/Controllers/StatsController.cs
@@ -228,7 +228,7 @@ namespace Werewolf_Web.Controllers
         }
 
         [HttpGet]
-        public JsonResult PlayerAchievements(int pid, bool json=false)
+        public JsonResult PlayerAchievements(int pid)
         {
             using (var DB = new WWContext())
             {
@@ -240,21 +240,11 @@ namespace Werewolf_Web.Controllers
                 if (p.Achievements == null)
                     p.Achievements = 0;
                 var ach = (Achievements)p.Achievements;
-                if (!json)
-                {
-                    var reply = "<br/><table class=\"table table-hover\"><tbody>";
-                    foreach (var a in ach.GetUniqueFlags())
-                        reply += "<tr><td><b>" + a.GetName() + "</b></td><td>" + a.GetDescription() + "</td></tr>";
-                    reply += "</tbody></table>";
-                    return Json(reply, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    List<object> reply = new List<object>();
-                    foreach (var a in ach.GetUniqueFlags())
-                        reply.Add(new { name = a.GetName(), description = a.GetDescription() });
-                    return Json(reply, JsonRequestBehavior.AllowGet);
-                }
+                var reply = "<br/><table class=\"table table-hover\"><tbody>";
+                foreach (var a in ach.GetUniqueFlags())
+                    reply += "<tr><td><b>" + a.GetName() + "</b></td><td>" + a.GetDescription() + "</td></tr>";
+                reply += "</tbody></table>";
+                return Json(reply, JsonRequestBehavior.AllowGet);
             }
         }
 
