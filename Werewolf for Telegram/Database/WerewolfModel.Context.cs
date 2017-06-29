a73cf01b90d0391a6a42c99019fe2fa4fd36430d
renyhp <renyhp@hotmail.it>
2017-06-29 16:47:20 +0200

    Revert last commit for now
    This reverts commit ab6149eb605f891673f2e2e2d164c5df1f0dc86d.

--- Werewolf for Telegram/Database/WerewolfModel.Context.cs
+++ Werewolf for Telegram/Database/WerewolfModel.Context.cs
@@ -53,7 +53,6 @@ namespace Database
         public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
         public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
         public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
-        public virtual DbSet<GroupPoint> GroupPoints { get; set; }
     
         public virtual ObjectResult<getPlayTime_Result> getPlayTime(Nullable<int> playerCount)
         {
