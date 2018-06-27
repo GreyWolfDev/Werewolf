23fe006189f5bb69c277df27376336218674f634
Jeff <jeffchoy94610@gmail.com>
2018-06-27 11:02:03 +0800

    update persian support chat id

--- Werewolf for Telegram/Werewolf Control/Helpers/Settings.cs
+++ Werewolf for Telegram/Werewolf Control/Helpers/Settings.cs
@@ -28,7 +28,7 @@ namespace Werewolf_Control.Helpers
 
 
         public static string TcpSecret => Environment.MachineName.GetHashCode().ToString();
-        public static readonly long PersianSupportChatId = -1001059174638;
+        public static readonly long PersianSupportChatId = -1001398432551;
         public static readonly long MainChatId = -1001049529775; //Beta group
         public static readonly long SupportChatId = -1001060486754; //@werewolfsupport
         public static readonly long PrimaryChatId = -1001268085464; //@werewolfgame
