7693612cedb581b4e4e623a3f85cef7056886c5d
Olgabrezel <lumpes8524@gmail.com>
2018-07-23 22:45:50 +0200

    Revert "Move Node and ClearUpdates from Telegram.Bot.Core to Telegram.Bot... let's test it xD"
    This reverts commit 538ede69d89d70f3743ec756030277b4e04a4348.

--- Werewolf for Telegram/Werewolf Node/Program.cs
+++ Werewolf for Telegram/Werewolf Node/Program.cs
@@ -322,7 +322,7 @@ namespace Werewolf_Node
             //message = message.Replace("`",@"\`");
             if (clearKeyboard)
             {
-                var menu = new ReplyKeyboardRemove();
+                var menu = new ReplyKeyboardRemove() { RemoveKeyboard = true };
                 return await Bot.SendTextMessageAsync(id, message, replyMarkup: menu, disableWebPagePreview: true, parseMode: ParseMode.Html, disableNotification: notify);
             }
             else if (customMenu != null)
