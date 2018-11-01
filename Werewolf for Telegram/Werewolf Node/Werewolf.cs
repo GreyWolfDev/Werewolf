c0cbae7042e04828e80ca5c9b63f542dfaf2242b
Jeff <jeffchoy94610@gmail.com>
2018-11-01 13:51:54 +0800

    one more day for spumpkin

--- Werewolf for Telegram/Werewolf Node/Werewolf.cs
+++ Werewolf for Telegram/Werewolf Node/Werewolf.cs
@@ -1515,7 +1515,8 @@ namespace Werewolf_Node
                 // special roles for events
                 // halloween this time
                 var toBeReplaced = new[] { IRole.Mason, IRole.Cupid, IRole.Villager, IRole.Pacifist, IRole.Sandman };
-                if ((DateTime.UtcNow.AddHours(14).Date == new DateTime(2018, 10, 31) || DateTime.UtcNow.AddHours(-11) == new DateTime(2018, 10, 31)))
+                var availableDates = new[] { new DateTime(2018, 10, 31), new DateTime(2018, 11, 1) };
+                if (availableDates.Contains(DateTime.UtcNow.AddHours(14).Date) || availableDates.Contains(DateTime.UtcNow.Date) || availableDates.Contains(DateTime.UtcNow.AddHours(-11).Date))
                     if (rolesToAssign.Any(x => toBeReplaced.Contains(x)))
                         rolesToAssign[rolesToAssign.IndexOf(rolesToAssign.First(x => toBeReplaced.Contains(x)))] = IRole.Spumpkin;
 
