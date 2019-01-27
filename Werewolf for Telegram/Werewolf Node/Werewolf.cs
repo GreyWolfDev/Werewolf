4b105d32d501844c49eb8ba8fe63a152a4e563f5
Olgabrezel <lumpes8524@gmail.com>
2019-01-27 16:32:25 +0100

    Undo new cupid mechanic

--- Werewolf for Telegram/Werewolf Node/Werewolf.cs
+++ Werewolf for Telegram/Werewolf Node/Werewolf.cs
@@ -4704,13 +4704,6 @@ namespace Werewolf_Node
                         var p = GetDBGamePlayer(w, db);
                         p.Won = true;
                     }
-                    var cupids = Players.Where(x => x.PlayerRole == IRole.Cupid && !x.InLove); // include dg cupid
-                    foreach (var cupid in cupids)
-                    {
-                        cupid.Won = true;
-                        var p = GetDBGamePlayer(cupid, db);
-                        p.Won = true;
-                    }
                 }
                 else
                 {
