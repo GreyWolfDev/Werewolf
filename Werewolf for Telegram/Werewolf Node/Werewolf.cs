015a9ce0dd6bd61d61e307d710eefbb2393c0353
Florian Meyer <florian.meyer@capgemini.com>
2019-02-18 18:05:49 +0100

    added teams for the roles

--- Werewolf for Telegram/Werewolf Node/Werewolf.cs
+++ Werewolf for Telegram/Werewolf Node/Werewolf.cs
@@ -1697,6 +1697,8 @@ namespace Werewolf_Node
                     case IRole.Detective:
                     case IRole.Gunner:
                     case IRole.Spumpkin:
+                    case IRole.Augur:
+                    case IRole.GraveDigger:
                         p.Team = ITeam.Village;
                         break;
                     case IRole.Doppelgänger:
@@ -1719,6 +1721,9 @@ namespace Werewolf_Node
                         break;
                     case IRole.SerialKiller:
                         p.Team = ITeam.SerialKiller;
+                        break;
+                    case IRole.Arsonist:
+                        p.Team = ITeam.Arsonist;
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
