e0bb5d8732c60eb41fe7faf1d96ceefafc79c65e
Justin Mangum <parabola949@gmail.com>
2020-03-24 15:44:33 -0500

    ONE MORE

--- Werewolf for Telegram/Werewolf Control/Handlers/UpdateHandler.cs
+++ Werewolf for Telegram/Werewolf Control/Handlers/UpdateHandler.cs
@@ -1087,25 +1087,35 @@ namespace Werewolf_Control.Handler
                             {
                                 using (var db = new WWContext())
                                 {
-                                    var para = db.Players.FirstOrDefault(x => x.Id == userid);
+                                    var para = db.Players.FirstOrDefault(x => x.Id == userid).Id;
 
                                     //get all the players Para has played with
                                     var ohaiplayers = (from g in db.Games
                                                        join gp in db.GamePlayers on g.Id equals gp.GameId
                                                        join gp2 in db.GamePlayers on g.Id equals gp2.GameId
                                                        join pl in db.Players on gp2.PlayerId equals pl.Id
-                                                       where gp.PlayerId == para.Id
-                                                       select pl).Distinct();
+                                                       where gp.PlayerId == para
+                                                       select pl).Distinct().Select(x => new { x.Id, x.NewAchievements }).ToList();
 
+                                    //now filter
+                                    for(var i = ohaiplayers.Count - 1; i >= 0; i--)
+                                    {
+                                        var p = ohaiplayers[i];
+                                        var ach = p.NewAchievements == null ? new BitArray(200) : new BitArray(p.NewAchievements);
+                                        if (ach.HasFlag(AchievementsReworked.OHAIDER))
+                                            ohaiplayers.RemoveAt(i);
+                                    }
+                                    
                                     //figure out which players don't have the achievement
 
                                     //update the message
-                                    var ohaimsg = $"Found {ohaiplayers.Count()} players that have earned OHAIDER.";
+                                    var ohaimsg = $"Found {ohaiplayers.Count()} new players that have earned OHAIDER.";
                                     Bot.Edit(query, ohaimsg);
                                     var count = 0;
                                     foreach (var player in ohaiplayers)
                                     {
-                                        if (AddAchievement(player, AchievementsReworked.OHAIDER, db))
+                                        var p = db.Players.FirstOrDefault(x => x.Id == player.Id);
+                                        if (AddAchievement(p, AchievementsReworked.OHAIDER, db))
                                             count++;
                                         Thread.Sleep(200);
                                     }
